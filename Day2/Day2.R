eac417d6bc114d046f017e232e2ac3ea1d7a756c
Mark Dirksen <mark.dirksn@ebpi.nl>
2018-12-03 20:57:46 +0100

    part 2 day 2

--- Day2/Day2.R
+++ Day2/Day2.R
@@ -5,6 +5,8 @@ library(stringr)
 data_file_location <- "C:/code/AdventOfCode/Day2/data.txt"
 IDs <- as.vector(unlist(read.table(data_file_location),use.names = FALSE))
 
+#part 1
+
 #define some variables
 
 alphabet <- c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
@@ -41,6 +43,42 @@ for (i in 1:length(IDs)){
 checksums <- sum(two)*sum(three)
 
 print(checksums)
+
+
+#part 2
+
+compare_string <- matrix(0, length(IDs), length(IDs))
+
+
+for (i in 1:length(IDs)){
+  for (j in 1:length(IDs)){
+    n <- 0
+    for (k in 1:nchar(IDs[j])){
+      if(i==j) break
+      char1 <- substring(IDs[i],k,k)
+      char2 <- substring(IDs[j],k,k)
+      if (char1!=char2) n <- n+1
+      if (n==2) break
+      if (k==nchar(IDs[j])) compare_string[i,j] <- 1
+    }
+  }
+}
+
+compare_sum <- apply(compare_string,1,sum)
+
+nmbrs <- IDs[which(compare_sum==1)]
+
+str <- ""
+
+for (k  in 1:nchar(IDs[1])){
+  char1 <- substring(nmbrs[1],k,k)
+  char2 <- substring(nmbrs[2],k,k)
+  if (char1==char2) str <- paste(str, char1, sep="")
+}
+
+print(str)
+
+
 
 
 
