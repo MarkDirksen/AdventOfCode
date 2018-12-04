d1e357fd91a7836b537859627e037886d6bebecc
Mark Dirksen <mark.dirksn@ebpi.nl>
2018-12-04 11:20:50 +0100

    fix on script day 3 part 1

--- Day3/Cloth.R
+++ Day3/Cloth.R
@@ -1,6 +1,6 @@
-n <- 1000
+n <- 1100
 data_file_location <- "C:/code/AdventOfCode/Day3/data.txt"
-cloth_list <- read.table(data_file_location,header=FALSE,sep=" ")
+cloth_list <- read.table(data_file_location,header=FALSE,sep=" ",stringsAsFactors = FALSE)
 cloth_list <- cloth_list[,c(3,4)]
 
 cloth_list[,1] <- sub(":","",cloth_list[,1])
@@ -9,12 +9,14 @@ c2 <- sub(",.*","",cloth_list[,1])
 c3 <- sub(".*x","",cloth_list[,2])
 c4 <- sub("x.*","",cloth_list[,2])
 
-cloth_list <- data.matrix(data.frame(c1,c2,c3,c4))
+cloth_list <- matrix(as.numeric(c(c2,c1,c4,c3)),ncol=4)
 
 max_width <- max(apply(cloth_list,1,function(x) x[[1]]+x[[3]]))
 max_height <- max(apply(cloth_list,1,function(x) x[[2]]+x[[4]]))
 
 print(c(max_width,max_height))
+
+#part1
 
 Grid <- matrix(0, n, n)
 
@@ -37,4 +39,8 @@ for(row in 1:nrow(Grid)) {
 
 sum(Grid)
 
+#111708 too low
+
 image(Grid)
+
+#part2
