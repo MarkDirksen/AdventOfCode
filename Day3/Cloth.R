f9b1796eb8fa5217da921dcb8b302b75cf0903ce
Mark Dirksen <mark.dirksn@ebpi.nl>
2018-12-04 10:38:48 +0100

    part1

---
+++ Day3/Cloth.R
@@ -0,0 +1,40 @@
+n <- 1000
+data_file_location <- "C:/code/AdventOfCode/Day3/data.txt"
+cloth_list <- read.table(data_file_location,header=FALSE,sep=" ")
+cloth_list <- cloth_list[,c(3,4)]
+
+cloth_list[,1] <- sub(":","",cloth_list[,1])
+c1 <- sub(".*,","",cloth_list[,1])
+c2 <- sub(",.*","",cloth_list[,1])
+c3 <- sub(".*x","",cloth_list[,2])
+c4 <- sub("x.*","",cloth_list[,2])
+
+cloth_list <- data.matrix(data.frame(c1,c2,c3,c4))
+
+max_width <- max(apply(cloth_list,1,function(x) x[[1]]+x[[3]]))
+max_height <- max(apply(cloth_list,1,function(x) x[[2]]+x[[4]]))
+
+print(c(max_width,max_height))
+
+Grid <- matrix(0, n, n)
+
+for (i in 1:dim(cloth_list)[1]){
+  vec <- cloth_list[i,]
+  for(j in (vec[1]+1):(vec[1]+vec[3])){
+    for(k in (vec[2]+1):(vec[2]+vec[4])){
+      Grid[j,k] <- Grid[j,k]+1
+    }
+  }
+}
+
+# Loop over Grid
+for(row in 1:nrow(Grid)) {
+  for(col in 1:ncol(Grid)) {
+    if(Grid[row, col]<=1) Grid[row, col] <- 0
+    else Grid[row, col] <- 1
+  }
+}
+
+sum(Grid)
+
+image(Grid)
