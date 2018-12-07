4609b23222df2c31a6dcb24a9e3c4ac1a7db282c
Mark Dirksen <mark.dirksn@ebpi.nl>
2018-12-07 17:47:27 +0100

    part 2

--- Day5/Polymer.R
+++ Day5/Polymer.R
@@ -33,3 +33,44 @@ print(result)
 
 nchar(result)
 
+#part 2
+
+alphabet_lower <- c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+alphabet_upper <- c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+polymer_result <- rep(0,26)
+
+for (k in 1:length(alphabet_lower)){
+  
+  stripped <- polymer[which(!(polymer==alphabet_lower[k]|polymer==alphabet_upper[k]))]
+  
+  i <-1
+  
+  while (TRUE){
+    letter1 <- stripped[i]
+    letter2 <- stripped[i+1]
+    if(grepl("^[[:upper:]]+$", (letter1))) { test <- tolower(letter1)==letter2
+    } else {test <- toupper(letter1)==letter2}
+    if (test==TRUE){
+      if(i==1) stripped <- stripped[(i+2):length(stripped)]
+      if(((i+1)!=length(stripped))&&(i!=1)) stripped <- stripped[c(1:(i-1),(i+2):length(stripped))]
+      if((i+1)==length(stripped)) stripped <- stripped[1:(i-1)]
+      if(i!=1) i <- i-1
+    } else {i <- i+1}
+    print(c(k,length(stripped)))
+    if (i==length(stripped)) break
+  }
+  
+  polymer_result[k] <- length(stripped)
+  
+}
+
+c(alphabet_lower[which(polymer_result==min(polymer_result))],min(polymer_result))
+
+
+
+
+
+
+
+
+
